feb15d2408c5ca9b542b830845d85258008ae5c0
Sakib Alhassan <sakibalhassan283@gmail.comgit config --global user.name>
2024-02-14 12:15:08 +0600

    added tree section

---
+++ script/alpha-clas.js
@@ -0,0 +1,8 @@
+function play() {
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+
+    // show play ground
+    const playGround = document.getElementById('paly-ground');
+    playGround.classList.remove('hidden');
+}
