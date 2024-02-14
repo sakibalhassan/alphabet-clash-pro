a8750616eb609e4fa436db9c5c97c3b45fd79d12
Sakib Alhassan <sakibalhassan283@gmail.comgit config --global user.name>
2024-02-14 15:40:48 +0600

    added alphabets

--- script/alpha-clas.js
+++ script/alpha-clas.js
@@ -1,8 +1,28 @@
-function play() {
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
+// function play() {
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
 
-    // show play ground
-    const playGround = document.getElementById('paly-ground');
-    playGround.classList.remove('hidden');
+//     // show play ground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+// }
+
+function continueGame() {
+    const alphabet = getRandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // display alphabet
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set bg color
+    setBacgrounColorById(alphabet);
+}
+
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
---
+++ script/utility.js
@@ -0,0 +1,32 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBacgrounColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+
+function getRandomAlphabet() {
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    // get random index between 0-25
+
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
+}
