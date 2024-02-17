d378899c5fb023b3847f565793021e682bb7fc13
Sakib Alhassan <sakibalhassan283@gmail.comgit config --global user.name>
2024-02-17 18:38:49 +0600

    added all project

--- script/alpha-clas.js
+++ script/alpha-clas.js
@@ -7,6 +7,57 @@
 //     playGround.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event) {
+    const playerPresses = event.key;
+    console.log('player pressed', playerPresses);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPresses, currentAlphabet);
+
+    if (playerPresses === expectedAlphabet) {
+        console.log('you get point');
+        // update score
+
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        const newScore = currentScore + 1;
+
+        console.log(newScore);
+
+        currentScoreElement.innerText = newScore;
+
+
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('you missed.you lost a life');
+
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+
+        currentLifeElement.innerText = newLife;
+    }
+    if (newLife === 0) {
+        console.log('game over')
+    }
+}
+
+
+// keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+
+
 function continueGame() {
     const alphabet = getRandomAlphabet();
     console.log('your random alphabet', alphabet);
@@ -18,11 +69,15 @@ function continueGame() {
 
     // set bg color
     setBacgrounColorById(alphabet);
+
+
+    // remove bg-color
+
 }
 
 
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
-    continueGame()
+    continueGame();
 }
--- script/utility.js
+++ script/utility.js
@@ -13,6 +13,11 @@ function setBacgrounColorById(elementId) {
     element.classList.add('bg-orange-400');
 }
 
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
 
 
 function getRandomAlphabet() {
